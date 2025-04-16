7c20621afa6f51301d146eea7d783c3c60874c59
shobhit2603 <shrivastavashobhit706@gmail.com>
2025-04-16 20:43:29 +0530

    Added Reviews to the Listings

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js")
 
 const listingSchema = new Schema({
     title: {
@@ -33,6 +34,19 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ]
+});
+
+//Review Deletion Middleware
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
